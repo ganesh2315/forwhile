5180c36671b8a96c450b1e7ad73ddf9068e44108
GANESH GHOSH <ganeshghosh02015@gmail.com>
2019-09-10 10:53:42 +0530

    forwhile

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i=0;
+
+        while (i<5){
+            System.out.println("counting::"+i);
+            i++;
+        }
+
+       /* for(int i=0;i<10;i++){
+            if (i%2==0) {
+                System.out.println("counting::" + i);
+            }
+        }*/
+    }
+}
